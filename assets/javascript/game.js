7d42cca517b50b173c06dbfc3449adacca13e6b0
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-08-31 11:34:11 -0400

    added pseudocode to javascript file.

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+// Declare global variables
+
+// Create for loop to dynamically create img tags for crystal images and assign random value to each
+
+// Create onclick listener for crystal images
+// When each is clicked, pull value attr, parse into integer, and add this to the counter
+
+
+// Create conditionals to chck if player has met target number of not (win/loss)
