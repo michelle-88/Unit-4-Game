51ce545cbf170bc8a3900491bae4ec472bc3a319
Michelle M <53539385+michelle-88@users.noreply.github.com>
2019-08-31 12:21:24 -0400

    created onclick listener for crystal images. when a crystal is clicked, the function will pull the value attribute of the crystal, parse it to an integer, and add the value to the counter. the updated counter will then be displayed on index page.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -21,10 +21,24 @@ for(var i = 0; i < numberOptions.length; i++){
     var imageCrystal = $("<img>");
     imageCrystal.addClass("crystal-image");
     imageCrystal.attr("src", "https://vignette.wikia.nocookie.net/jumpingground/images/0/06/Time_stone.png/revision/latest?cb=20180916204409");
-    imageCrystal.attr("crystalvalue", numberOptions[i]);
+    imageCrystal.attr("num-crystalvalue", numberOptions[i]);
     $("#crystals").append(imageCrystal);
 };
+
 // Create onclick listener for crystal images
+$(".crystal-image").on("click", function(){
+
+    // Create variable that will store value of crystal that was clicked on and add this to player total score
+    var crystalValue = ($(this).attr("num-crystalvalue"));
+    crystalValue = parseInt(crystalValue);
+    counter += crystalValue;
+
+    // Display player total score so far on index page
+    $("#scoreSoFar").text(counter);
+});
+
+
+
 // When each is clicked, pull value attr, parse into integer, and add this to the counter
 
 
